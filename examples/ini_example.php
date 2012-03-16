5ec13f4690b65dfbefa792c9fbdfcab9c8767475
Bryan Davis <bpd@keynetics.com>
2012-03-16 07:28:56 -0700

    Updated examples.

--- examples/ini_example.php
+++ examples/ini_example.php
@@ -1,7 +1,20 @@
 <?php
+/**
+ * Example usage of ini file graph template with variable expansion.
+ */
+
 require_once dirname(__FILE__) . '/../src/autoload.php';
 
-$g = new Graphite_GraphBuilder('cpu_irq.ini',
-    array('width' => 800, 'height' => 400),
-    array( 'hostname' => 'com.example.foo'));
-echo $g->url, "\n";
+$g = new Graphite_GraphBuilder();
+$g->prefix('com.example.host')
+  ->ini('interface.ini', array('IF' => 'Tunnel0', 'IF_DESC' => 'tu0'));
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  <head>
+  <body>
+    <img src="http://graphite.example.com/render?<?php echo $g->url(); ?>">
+  </body>
+</html>
